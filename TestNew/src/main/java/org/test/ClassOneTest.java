3b88e999dae24dc10c055d515d8df37bc535f411
prithiv <prithiv@DESKTOP-JEESU72>
2024-07-16 21:38:17 +0530

    First Commit

---
+++ TestNew/src/main/java/org/test/ClassOneTest.java
@@ -0,0 +1,14 @@
+package org.test;
+
+import org.test.ClassOneTest;
+
+public class ClassOneTest {
+	private void methodOne() {
+		System.out.println("Amor");
+	}
+	public static void main(String[] args) {
+		ClassOneTest one = new ClassOneTest();
+		one.methodOne();
+	}
+	}
+
